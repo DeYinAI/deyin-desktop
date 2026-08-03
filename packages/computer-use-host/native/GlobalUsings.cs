6f661bbd59840eeb0a97fd992b3aa107ed07e1e2
Anh Tran <anhlead@outlook.com>
2026-08-03 11:59:34 +0100

    fix: add GlobalUsings.cs for System.IO namespace
    When UseWPF is enabled, implicit usings don't include System.IO by default.
    Add explicit global usings for System.IO and other common namespaces to fix
    compilation errors for Path, Directory, StreamReader, and SearchOption types.

---
+++ packages/computer-use-host/native/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives for the computer-use-host project
+global using System;
+global using System.IO;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using System.Linq;
